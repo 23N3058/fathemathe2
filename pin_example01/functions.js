dbd021fe70a66daea58e0ff8c6e4d798269077f5
23N3058 <yusuke.nitta.8m@stu.hosei.ac.jp>
2024-12-14 16:21:28 +0900

    fathmath

---
+++ pin_example01/functions.js
@@ -0,0 +1,31 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const imageContainer=document.querySelectorAll('.image-container');
+const layer=document.querySelectorAll('.image>*');
+
+gsap.set('#layer02,#layer03,#layer04,#bunshou2',{opacity:0});
+gsap.set('#layer01,bunshou',{opacity:1});
+
+ScrollTrigger.create({
+    trigger:imageContainer,
+    start:'top top',
+    end:'+=2000',
+    pin:true,
+    scrub:true,
+//     markers:true,
+    onUpdate:(self)=>{
+        if(self.progress<0.25){
+            gsap.to('#layer01,#bunshou',{opacity:1});
+            gsap.to('#layer02,#layer03,#layer04,#bunshou2',{opacity:0});
+        }else if(self.progress>=0.25 && self.progress<0.5){
+            gsap.to('#layer01,#layer02,#bunshou',{opacity:1});
+            gsap.to('#layer03,#layer04,#bunshou2',{opacity:0});
+        }else if(self.progress>=0.5&&self.progress<0.75){
+            gsap.to('#layer01,#layer02,#layer03,#bunshou2',{opacity:1});
+            gsap.to('#layer04,#bunshou',{opacity:0});
+        }else{
+            gsap.to('#layer01,#layer02,#layer03,#layer04,#bunshou2',{opacity:1});
+            gsap.to('#bunshou',{opacity:0});
+        }
+        },
+});
